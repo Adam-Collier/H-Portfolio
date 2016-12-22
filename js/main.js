5d998c7641d16dae4d122b8d7d677e10da5948d5
Adam-Collier <adamcollierart@gmail.com>
2016-12-22 15:32:31 +0000

    link paths and new images

--- js/main.js
+++ js/main.js
@@ -5,19 +5,12 @@ $(function() {
     var images = ['web1', 'web2', 'web3', 'web4', 'web5'];
     var max = images.length;
 
-    // Get current image src
-    var curSrc = $('#imageswap').attr('src');
+
 
 
     var curIdx = 0;
 
-    // Search image list for index of current ID
-    while (curIdx < max) {
-        if (images[curIdx] === curSrc) {
-            break;
-        }
-        curIdx++;
-    }
+
 
     // Next image on button (and image) click
     $('.right').click( function() {
